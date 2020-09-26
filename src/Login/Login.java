413e2355d54a84a77441ede65a1fd797bc0f16ae
Erinxon <erinxons@gmail.com>
2020-09-26 14:14:11 -0400

    3 commit

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -204,7 +204,7 @@ public class Login extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private  void inciar(){
+    private  void iniciar(){
         try {
             this.usuario = this.textUsuario.getText();
             this.password = this.textPass.getText();
@@ -247,7 +247,7 @@ public class Login extends javax.swing.JFrame {
 
     private void btnInicarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInicarSesionActionPerformed
 
-        inciar();
+        iniciar();
 
     }//GEN-LAST:event_btnInicarSesionActionPerformed
 
@@ -266,7 +266,7 @@ public class Login extends javax.swing.JFrame {
 
     private void textPassKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_textPassKeyPressed
         if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            inciar();
+            iniciar();
         }
 
     }//GEN-LAST:event_textPassKeyPressed
